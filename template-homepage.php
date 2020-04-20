8725c455a58c1891b7606578a93b37708f24df08
TestSubject45 <blakegambel@yahoo.com>
2020-04-20 16:58:23 -0600

    Added page content section

--- template-homepage.php
+++ template-homepage.php
@@ -2,7 +2,17 @@
 
 <?php get_header(); ?>
 <main id="content">
+<!-- Display Nav -->
 <?php get_template_part( 'nav', 'below' ); ?>
+<!-- Get page content -->
+<?php if ( have_posts() ) :
+	// Start the Loop.
+	while ( have_posts() ) : the_post(); ?>
+        <div>
+            <div id="content-area">
+				<?php the_content(); ?>
+            </div>
+        </div>
+<?php endwhile;endif; ?>  
 </main>
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
