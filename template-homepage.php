e5d9a577c7148bf533c45a65240392aef589dd60
TestSubject45 <blakegambel@yahoo.com>
2020-04-20 16:00:18 -0600

    created homepage template

---
+++ template-homepage.php
@@ -0,0 +1,8 @@
+<?php /* Template Name: Homepage */ ?>
+
+<?php get_header(); ?>
+<main id="content">
+<?php get_template_part( 'nav', 'below' ); ?>
+</main>
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
