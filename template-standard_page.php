1dd54e615fd6efa094f1f92b2520747ad407259c
TestSubject45 <blakegambel@yahoo.com>
2020-05-18 17:13:50 -0600

    Update template-standard_page.php

--- template-standard_page.php
+++ template-standard_page.php
@@ -4,9 +4,16 @@
 <!-- Display Nav -->
 <?php get_template_part( 'nav', 'below' ); ?>
 <main id="content">
-<!-- Get page content -->
-<div id="content-area">
-	<?php the_content(); ?>
-</div>
+<!-- Page Title -->
+<h1 class="entry-title"><?php the_title(); ?></h1> 
+<!-- Page Content -->
+    <?php
+    while ( have_posts() ) : the_post(); ?> <!--Because the_content() works only inside a WP Loop -->
+            <?php the_content(); ?> <!-- Page Content -->
+
+    <?php
+    endwhile; //resetting the page loop
+    wp_reset_query(); //resetting the page query
+    ?>
 </main>
 <?php get_footer(); ?>
