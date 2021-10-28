f24a29aee514ad0e9c89186814ccab254f783bde
TestSubject45 <blakegambel@yahoo.com>
2021-10-28 15:40:20 -0600

    Added new Google Fonts

--- header.php
+++ header.php
@@ -4,7 +4,10 @@
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <meta name="viewport" content="width=device-width" />
 <!--Google Fonts -->
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Cousine:ital,wght@0,400;0,700;1,400;1,700&family=Inconsolata:wght@400;700&family=Oxygen+Mono&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Coda:wght@400;800&display=swap" rel="stylesheet">
 <!--End Google Fonts -->
 <?php wp_head(); ?>
 </head>
